3ab1c7afebe064e6553dbcc6afbd91b02f8bdf3f
Prybrt06 <prybrt06@gmail.com>
2022-07-04 15:13:46 +0530

    Firestore activated

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,16 +1,15 @@
 import UIKit
 import FirebaseAuth
+import FirebaseCore
+import FirebaseFirestore
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var messages: [Message] = [
-        Message(sender: "1@2.com", body: "Hey!"),
-        Message(sender: "a@b.com", body: "After a user signs in for the first time, a new user account is created and linked to the credentials—that is, the user name and password, phone number, or auth provider information—the user signed in with. This new account is stored as part of your Firebase project, and can be used to identify a user across every app in your project, regardless of how the user signs in.!"),
-        Message(sender: "1@2.com", body: "What's up?"),
-    ]
+    let db = Firestore.firestore()
+    var messages: [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +20,58 @@ class ChatViewController: UIViewController {
         navigationItem.hidesBackButton = true
         
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
+        
+    }
+    
+    func loadMessages()
+    {
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField, descending: false)
+            .addSnapshotListener { (querySnapshot, error) in
+            
+            self.messages = []
+            
+            if let e = error {
+                print("There was an issue retriving data from FireStore. \(e)")
+            }
+            
+            else
+            {
+                if let snapShotDocuments = querySnapshot?.documents {
+                    for doc in snapShotDocuments {
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender,
+                                                                      K.FStore.bodyField: messageBody,
+                                                                      K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to firestore, \(e)")
+                }
+                else {
+                    print("Successfully saved data.")
+                }
+            }
+        }
+        
+        messageTextfield.text = ""
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -50,6 +98,10 @@ extension ChatViewController: UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let message = messages[indexPath.row]
+        
+        if message.sender == Auth.auth().currentUser?.email
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         cell.lebel.text = messages[indexPath.row].body
         
--- Podfile
+++ Podfile
@@ -7,5 +7,6 @@ target 'Flash Chat iOS13' do
   
   pod 'FirebaseAuth'
   pod 'FirebaseFirestore'
+  pod 'IQKeyboardManagerSwift', '6.5.10'
   
 end
